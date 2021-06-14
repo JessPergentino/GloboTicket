5e727f4cb47b71545c95672a4bea8ba02da6517a
Jessica Pergentino <jessicapergentino@hotmail.com>
2021-06-14 19:56:31 -0300

    Module 5 - Routing Between Components

--- Home.js
+++ Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image, Text, View, StyleSheet } from 'react-native';
-
+import Menu from './Menu';
 
 const Home = (props) => {
     return (
@@ -11,25 +11,34 @@ const Home = (props) => {
             />
             <Text style={styles.title}>Welcome TO GloboTicket</Text>
             <Text style={styles.subtitle}>{props.username}</Text>
+            <Image
+                style={styles.heroimage}
+                source={require('./images/boxing.jpg')}
+            />
 
             <View style={styles.textcontainer}>
                 <Text style={styles.content}>{introText}</Text>
+            </View>
+
+            <View style={styles.menu}>
+                <Menu />
             </View>
         </View>
     );
 }
 
-const introText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ullamcorper elementum turpis bibendum pulvinar. Quisque sed sodales augue. Praesent a libero sit amet magna imperdiet congue. Fusce orci mi, ultricies sit amet urna ut, eleifend placerat turpis. Aenean elementum leo mauris, ac consectetur leo lobortis in. Etiam dignissim in odio nec viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod erat non augue faucibus, tincidunt fringilla mauris volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer velit velit, malesuada vitae cursus id, tempus eu erat. Sed vestibulum ligula sapien, a sollicitudin lorem ullamcorper sed. Praesent fringilla elit orci.`
+const introText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ullamcorper elementum turpis bibendum pulvinar. Quisque sed sodales augue. Praesent a libero sit amet magna imperdiet congue. Fusce orci mi, ultricies sit amet urna ut, eleifend placerat turpis. Aenean elementum leo mauris, ac consectetur leo lobortis in. Etiam dignissim in odio nec viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit.`
 
 const styles = StyleSheet.create({
     container: {
         flexDirection: "column",
         alignItems: "center",
-        padding: 20,
+        paddingTop: 20,
+        paddingBottom: 20,
+        flex: 1
     },
     textcontainer: {
-        textAlign: "center",
-        paddingTop: 10,
+        padding: 20,
     },
     globologo: {
         height: 100,
@@ -45,6 +54,14 @@ const styles = StyleSheet.create({
     content: {
         fontFamily: 'Ubuntu-Light',
         fontWeight: "300"
+    },
+    heroimage: {
+        height: 170,
+        width: '100%'
+    },
+    menu: {
+        position: 'absolute',
+        bottom: 10
     }
 })
 
---
+++ TicketsDB.js
@@ -0,0 +1,44 @@
+const globkTickets = [
+    {
+        event: 'DJ Conferemce',
+        image: require('./images/dj1.jpg'),
+        eventID: 'DJ-1',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed elit eleifend, pellentesque lacus placerat, eleifend risus. Integer commodo eros justo, id iaculis ipsum semper quis. Ut maximus sagittis risus vehicula dapibus. Ut quis tortor vestibulum, euismod elit nec, iaculis ex. Pellentesque neque nulla, sagittis et scelerisque vitae, cursus nec.`,
+        shortDescription: 'scelerisque fermentum dui faucibus in ornare quam',
+        price: 30.00,
+    },
+    {
+        event: 'Ultrices Gravida Dictum',
+        image: require('./images/evt2.jpg'),
+        eventID: 'EVT-2',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed elit eleifend, pellentesque lacus placerat, eleifend risus. Integer commodo eros justo, id iaculis ipsum semper quis. Ut maximus sagittis risus vehicula dapibus. Ut quis tortor vestibulum, euismod elit nec, iaculis ex. Pellentesque neque nulla, sagittis et scelerisque vitae, cursus nec.`,
+        shortDescription: 'scelerisque fermentum dui faucibus in ornare quam',
+        price: 70.00,
+    },
+    {
+        event: 'In Nisl',
+        image: require('./images/evt3.jpg'),
+        eventID: 'EVT-3',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed elit eleifend, pellentesque lacus placerat, eleifend risus. Integer commodo eros justo, id iaculis ipsum semper quis. Ut maximus sagittis risus vehicula dapibus. Ut quis tortor vestibulum, euismod elit nec, iaculis ex. Pellentesque neque nulla, sagittis et scelerisque vitae, cursus nec.`,
+        shortDescription: 'scelerisque fermentum dui faucibus in ornare quam',
+        price: 45.00,
+    },
+    {
+        event: 'Rhoncus Aenean',
+        image: require('./images/evt4.jpg'),
+        eventID: 'EVT-4',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed elit eleifend, pellentesque lacus placerat, eleifend risus. Integer commodo eros justo, id iaculis ipsum semper quis. Ut maximus sagittis risus vehicula dapibus. Ut quis tortor vestibulum, euismod elit nec, iaculis ex. Pellentesque neque nulla, sagittis et scelerisque vitae, cursus nec.`,
+        shortDescription: 'scelerisque fermentum dui faucibus in ornare quam',
+        price: 230.00,
+    },
+    {
+        event: 'Facilisis Mauris',
+        image: require('./images/evt5.jpg'),
+        eventID: 'EVT-5',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed elit eleifend, pellentesque lacus placerat, eleifend risus. Integer commodo eros justo, id iaculis ipsum semper quis. Ut maximus sagittis risus vehicula dapibus. Ut quis tortor vestibulum, euismod elit nec, iaculis ex. Pellentesque neque nulla, sagittis et scelerisque vitae, cursus nec.`,
+        shortDescription: 'scelerisque fermentum dui faucibus in ornare quam',
+        price: 160.00,
+    },
+]
+
+export default globkTickets;
